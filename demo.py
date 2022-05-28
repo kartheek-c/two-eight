a107556c1ccaebc4d43a0456f3d2e9dbd828a871
Poola Siva Kartheek <poola.siva.kartheek@ibm.com>
2022-05-28 18:47:04 +0530

    firstcommit

---
+++ demo.py
@@ -0,0 +1,10 @@
+def even(num):
+    if num%2==0:
+       print(f'{num} it is a even num')
+    else
+       print(f'{num} it is odd numbers')
+    return num
+
+data = int(input('enter a number'))
+result = even(data)
+
